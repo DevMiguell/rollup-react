6771594c59bc71c17e7b539b6c5ad749c3d73107
DevMiguell <buguel2000@gmail.com>
2022-01-25 08:22:47 -0300

    feat: React rolup create

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,11 @@
 import serve from "rollup-plugin-serve";
 import livereload from "rollup-plugin-livereload";
-import babel from '@rollup/plugin-babel';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import replace from '@rollup/plugin-replace';
-import image from '@rollup/plugin-image';
-import postcss from 'rollup-plugin-postcss';
+import babel from "@rollup/plugin-babel";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import replace from "@rollup/plugin-replace";
+import image from "@rollup/plugin-image";
+import postcss from "rollup-plugin-postcss";
 
 export default {
   input: "src/index.js",
@@ -23,7 +23,7 @@ export default {
       extensions: [".js"],
     }),
     replace({
-      'process.env.NODE_ENV': JSON.stringify( 'development' )
+      "process.env.NODE_ENV": JSON.stringify("development"),
     }),
     babel({
       presets: ["@babel/preset-react"],
@@ -37,5 +37,5 @@ export default {
       port: 3000,
     }),
     livereload({ watch: "dist" }),
-  ]
+  ],
 };
